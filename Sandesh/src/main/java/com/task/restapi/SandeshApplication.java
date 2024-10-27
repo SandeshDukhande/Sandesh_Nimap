fd2906174f350768fe0ad786cbab59e3df3a311d
SandeshDukhande <dukhandes25@gmail.com>
2024-10-27 15:09:20 +0530

    Project

---
+++ Sandesh/src/main/java/com/task/restapi/SandeshApplication.java
@@ -0,0 +1,13 @@
+package com.task.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandeshApplication.class, args);
+	}
+
+}
---
+++ Sandesh/src/test/java/com/task/restapi/SandeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
